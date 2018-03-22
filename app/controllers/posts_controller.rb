54d26bf25e85524ca70fd498695022da0593d05a
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-22 10:39:40 -0300

    Added authorization to user only update or delete your own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,20 +27,30 @@ class PostsController < ApplicationController
   def edit; end
 
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'O Post foi atualizado com sucesso!' }
-      else
-        format.html { render :edit, notice: 'O post não pode ser atualizado!' }
+    if validate_user(@post.user_id)
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post, notice: 'O Post foi atualizado com sucesso!' }
+        else
+          format.html { render :edit, notice: 'O post não pode ser atualizado!' }
+        end
       end
+    else
+      flash[:user_validation] = "Você não tem autorização para editar este post"
+      render :edit
     end
   end
 
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_path, notice: 'O post foi deletado.' }
-      format.json { head :no_content }
+    if validate_user(@post.user_id)
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_path, notice: 'O post foi deletado.' }
+        format.json { head :no_content }
+      end
+    else
+      flash[:user_validation] = "Você não tem autorização para remover este post"
+      redirect_to @post
     end
   end
 
