a513de12a603347ac1981ed8c2cd814058d40a02
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-22 09:04:33 -0300

    Implemented authorization to only create edit or delete posts/comments if user is logged in

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create edit destroy]
+
   def create
     @post = Post.friendly.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
