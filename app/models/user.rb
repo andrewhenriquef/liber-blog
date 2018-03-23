08df77f1f85f12781b1a6614611602a446bba735
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-23 11:55:18 -0300

    Created basic data valition test for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   validates :name, presence: { value: true, message: 'O perfil deve conter o nome' }
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
+
+  validates :password, length: { minimum: 6 }
+
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
