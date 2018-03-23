709fd4fb5b439c83f5855b696dc6c49df7b2e275
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-23 11:55:37 -0300

    Created basic data valition test for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   friendly_id :title, use: :slugged
 
   has_many :comments, dependent: :destroy
-  belongs_to :user
+  belongs_to :user, optional: false
 
   validates :title,       presence: { value: true, message: ': O titulo não foi preenchido' },
                           length: { maximum: 50, message: ': O titulo não pode ter mais de 50 caracteres' }
