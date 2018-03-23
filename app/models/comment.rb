12641567564f4fd96b4c4418c39442203873c1f4
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-03-23 12:01:46 -0300

    setup the user and post field optionality to false

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :user
+  belongs_to :post, optional: false
+  belongs_to :user, optional: false
 
   validates :body,  length:   { maximum: 250, message: ': O comentario não pode ter mais de 250 caracteres' },
                     presence: { value: true, message: ': O comentario está vazio' }
